1ffa21e52cdb97c673843a1bb2cd8d9658577715
Luis Pineda <luis@lpineda.dev>
2024-08-13 01:00:45 -0600

    Changing to postgres

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.NEON_URL
+    ? new Sequelize(process.env.NEON_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         // host: 'localhost',
-        host:   '127.0.0.1',
-        dialect: 'mysql',
-        port: 3306
+        host:   'localhost',
+        dialect: 'postgres',
+        port: 5432
     });
 
 module.exports = sequelize;
